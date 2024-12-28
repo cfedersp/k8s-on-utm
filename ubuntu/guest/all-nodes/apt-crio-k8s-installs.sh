272f2a71dbd4b223ce6cff736138e6b63bdc1abc
Charles Federspiel <charlesfederspiel@Charless-MacBook-Pro.local>
2024-12-28 12:15:13 -0800

    removed unsafeSkipCAVerification, create new token if list is empty

--- ubuntu/guest/all-nodes/apt-crio-k8s-installs.sh
+++ ubuntu/guest/all-nodes/apt-crio-k8s-installs.sh
@@ -5,8 +5,8 @@ apt install net-tools parted lvm2 netcat-openbsd zip git apt-transport-https ca-
 KUBERNETES_VERSION=1.28
 CRIO_VERSION=1.28.2
 
-cat $(dirname $0)/crio-prerelease.key | gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
-cat $(dirname $0)/kubernetes-release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+cat $(dirname $0)/../../host-prep/keys/crio/Release.key | gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+cat $(dirname $0)/../../host-prep/keys/kubernetes/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" | tee /etc/apt/sources.list.d/cri-o.list
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
